26247f44a5786ef8cd48898e4a26c2309363376a
ehsna <e24saeedi@gmail.com>
2022-04-17 11:01:48 -0700

    Move translate_key out of the input_loop function

--- terminal_mines/game_logic/keyboard_listener.py
+++ terminal_mines/game_logic/keyboard_listener.py
@@ -15,6 +15,30 @@ class ArrowKeyMapping(Enum):
     LEFT = "a"
     RIGHT = "d"
 
+def translate_key(ch):
+    # If it's an escape sequence grab the ascii char code
+    if len(ch) > 1:
+        escape_ord = ord(ch[0])
+        ch = ch[-1]
+    else:
+        escape_ord = None
+    if 32 <= ord(ch) <= 126:
+        # Only pass "regular" keys onto the handler
+        if escape_ord == 27 or escape_ord == 224:
+            # Translate the arrow key escape sequence into the mapped letter
+            if ch == "A" or ch == "H":
+                ch = ArrowKeyMapping.UP.value
+            elif ch == "B" or ch == "P":
+                ch = ArrowKeyMapping.DOWN.value
+            elif ch == "D" or ch == "K":
+                ch = ArrowKeyMapping.LEFT.value
+            elif ch == "C" or ch == "M":
+                ch = ArrowKeyMapping.RIGHT.value
+        return (ch.lower())
+    elif ord(ch) == 13:
+        return "\n"
+    elif ord(ch) == 27:
+        return None
 
 def input_loop(handler_func):
     """
@@ -25,35 +49,11 @@ def input_loop(handler_func):
             ch = click.getchar()
         except (KeyboardInterrupt, EOFError):
             return
-
-        # If it's an escape sequence grab the ascii char code
-        if len(ch) > 1:
-            escape_ord = ord(ch[0])
-            ch = ch[-1]
-        else:
-            escape_ord = None
-
-        if 32 <= ord(ch) <= 126:
-            # Only pass "regular" keys onto the handler
-
-            if escape_ord == 27 or escape_ord == 224:
-                # Translate the arrow key escape sequence into the mapped letter
-                if ch == "A" or ch == "H":
-                    ch = ArrowKeyMapping.UP.value
-                elif ch == "B" or ch == "P":
-                    ch = ArrowKeyMapping.DOWN.value
-                elif ch == "D" or ch == "K":
-                    ch = ArrowKeyMapping.LEFT.value
-                elif ch == "C" or ch == "M":
-                    ch = ArrowKeyMapping.RIGHT.value
-
-            handler_func(ch.lower())
-        elif ord(ch) == 13:
-            # Translate the enter key to a newline
-            handler_func("\n")
-        elif ord(ch) == 27:
-            # Exit on ESC
+        # key = translate_key(ch)
+        if translate_key(ch) is None:
             return
+        else:
+            handler_func(translate_key(ch))
 
 
 def demo_handler(key):
